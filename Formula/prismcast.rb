522cc49003b9dbc8810b84658fdb30601bf21d5a
hjdhjd <hjdhjd@users.noreply.github.com>
2026-02-15 23:50:17 +0000

    Update PrismCast to 1.3.2.

--- Formula/prismcast.rb
+++ Formula/prismcast.rb
@@ -3,12 +3,12 @@ require "language/node"
 class Prismcast < Formula
   desc "Browser-based live TV streaming server for Channels DVR and Plex"
   homepage "https://github.com/hjdhjd/prismcast"
-  url "https://registry.npmjs.org/prismcast/-/prismcast-1.3.1.tgz"
-  sha256 "3fc90e0f5343f379d9ced113f4ce6229a91266615b95f4958dcba51f0ed281bf"
+  url "https://registry.npmjs.org/prismcast/-/prismcast-1.3.2.tgz"
+  sha256 "32dcffbf1bb34b400c067431e28a069f49c8445a915553ee5bd892bd68b10286"
   license "ISC"
 
   livecheck do
-    url "https://registry.npmjs.org/prismcast/-/prismcast-1.3.1.tgz"
+    url "https://registry.npmjs.org/prismcast/-/prismcast-1.3.2.tgz"
     regex(/"version"\s*:\s*"v?(\d+(?:\.\d+)+)"/i)
   end
 
